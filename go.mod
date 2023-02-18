e58eb4ce192e76ecbfa3dafacbae6b4028a2a0d4
Stan Bennett <stanley.bennett82@gmail.com>
2023-02-18 12:26:56 -0600

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module email-analyzer
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"regexp"
+	"sort"
+)
+
+func main() {
+	emailPath := path.Join(os.Getenv("USERPROFILE"), "Documents\\email")
+	files, err := os.ReadDir(emailPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var addresses = make(map[string]int)
+	var rgx = regexp.MustCompile(`\((.*?)\)`)
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		rs := rgx.FindStringSubmatch(file.Name())
+		var length = len(rs)
+		if length == 0 {
+			continue
+		}
+
+		var address string
+		if length > 1 {
+			address = rs[1]
+		} else if length == 1 {
+			address = rs[0]
+		}
+
+		addresses[address] = addresses[address] + 1
+	}
+
+	keys := make([]string, 0, len(addresses))
+	for k := range addresses {
+		keys = append(keys, k)
+	}
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		return addresses[keys[i]] < addresses[keys[j]]
+	})
+
+	for _, k := range keys {
+		fmt.Println(k, addresses[k])
+	}
+}
